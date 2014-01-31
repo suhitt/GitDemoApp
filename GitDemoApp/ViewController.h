69cb72fd6006ccc02f0d33284475a859b4a5fac7
suhitt <suhit.patil@globallogic.com>
2014-01-31 19:18:08 +0530

    Initial Commit

---
+++ GitDemoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitDemoApp
+//
+//  Created by Globallogic on 31/01/14.
+//  Copyright (c) 2014 Globallogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitDemoAppTests/GitDemoAppTests.m
@@ -0,0 +1,34 @@
+//
+//  GitDemoAppTests.m
+//  GitDemoAppTests
+//
+//  Created by Globallogic on 31/01/14.
+//  Copyright (c) 2014 Globallogic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitDemoAppTests : XCTestCase
+
+@end
+
+@implementation GitDemoAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
